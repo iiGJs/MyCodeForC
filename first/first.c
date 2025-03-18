ec135d675e2ba2fdc2ed157bf5fb428ea77b9e6c
iiGJs <Luna0202GaoJing@outlook.com>
2025-03-18 22:07:06 +0800

    删除test项目，添加了书本对应篇目源码，并加注释

---
+++ first/first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void) /*一个简单的C程序*/
+{
+  int num; /*定义一个名为num的变量*/
+  num = 1; /*为num附一个值*/
+
+  printf("I am a simple "); /*使用printf()函数*/
+  printf("computer.\n"); /*使用printf()函数*/
+  printf("My favorite number is %d because it is first.\n", num); /*使用printf()函数*/
+  
+  getchar();/*等待用户按下一个键*/
+  return 0;/*给调用提供方返回一个数（暂时看做结束main函数的要求）*/
+}
